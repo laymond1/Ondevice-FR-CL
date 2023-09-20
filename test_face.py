f35ec28b5ca4f4b9f0daa7dca101fcea7079c6f1
laymond1 <costor107@gmail.com>
2023-09-20 15:51:07 +0900

    update test_face to log results

--- test_face.py
+++ test_face.py
@@ -1,6 +1,8 @@
 from sklearn import metrics # it needs to be imported first in Jetson Nano
+import argparse
 import os
 import pickle
+import psutil
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
@@ -13,8 +15,45 @@ from scenario.evaluators import *
 
 from utils import seedEverything, create_if_not_exists, AverageMeter
 
+import wandb
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('argument')
+
+    parser.add_argument('--seed', type=int, default=0, help='random seed')
+    parser.add_argument('--notes', type=str, default='')
+    parser.add_argument('--method', type=str, default='lwf')
+    parser.add_argument('--target_device', type=str, default='pc', choices=['pc', 'nano'])
+
+    parser.add_argument('--n_workers', type=int, default=0)
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--test_batch_size', type=int, default=256)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--n_epochs', type=int, default=5)
+
+    parser.add_argument('--save_path', type=str, default='save/casia_15')
+    parser.add_argument('--save_model', type=int, default=1, choices=[0, 1])
+
+    parser.add_argument('--nowand', type=int, default=0, choices=[0, 1])
+    parser.add_argument('--debug_mode', type=int, default=0, choices=[0, 1])
+
+    parser.add_argument('--wandb_project', type=str, default='nano')
+    parser.add_argument('--wandb_entity', type=str, default='laymond1')
+
+    args = parser.parse_args()
+
+    args.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    return args
+
 
 def main(args):
+    if not args.nowand:
+        assert wandb is not None, "Wandb not installed, please install it or run without wandb"
+        wandb.init(project=args.wandb_project, entity=args.wandb_entity, config=vars(args))
+        args.wandb_url = wandb.run.get_url()
+        
     # seed
     seedEverything(args.seed)
     
@@ -32,13 +71,13 @@ def main(args):
     
     # scenario
     train_scenario = ClassIncremental(
-        dataset=casia_dataset, n_tasks=casia_dataset._DEFAULT_N_TASKS, batch_size=args.batch_size, n_workers=0
+        dataset=casia_dataset, n_tasks=casia_dataset._DEFAULT_N_TASKS, batch_size=args.batch_size, n_workers=args.n_workers
     )
 
     # Verification Scenario: n_tasks arguments 필요 없음.
     # LFW Test Dataset
     lfw_test_scenario = VerificationScenario(
-        dataset=lfw_test_dataset, n_tasks=lfw_test_dataset._DEFAULT_N_TASKS, batch_size=args.batch_size, n_workers=0
+        dataset=lfw_test_dataset, n_tasks=lfw_test_dataset._DEFAULT_N_TASKS, batch_size=args.test_batch_size, n_workers=args.n_workers
     )   
     
     # 
@@ -65,6 +104,8 @@ def main(args):
         args.save_path = os.path.join(args.save_path, method.NAME)
         create_if_not_exists(args.save_path)
     
+    print(f"Number of tasks: {train_scenario.n_tasks} | Number of classes: {train_scenario.n_classes}")
+    
     metrics = []
     # train
     method.train()
@@ -75,10 +116,13 @@ def main(args):
         # pbar = tqdm(range(args.n_epochs))
         for epoch in range(args.n_epochs):
             #-- Start Iteration
+            batch_time = AverageMeter()
             losses = AverageMeter()
+            
             pbar = tqdm(train_loader)
+            end = time.time()
             for idx, (inputs, labels, task, not_aug_inputs) in enumerate(pbar):
-                if args.debug and idx > 5:
+                if args.debug_mode and idx > 5:
                     break
                 inputs, labels, not_aug_inputs = inputs.to(method.device), labels.to(method.device), not_aug_inputs.to(method.device)
 
@@ -88,7 +132,16 @@ def main(args):
                     loss = method.observe(inputs, labels, not_aug_inputs)
                 losses.update(loss, inputs.size(0))
                 
+                # measure elapsed time
+                batch_time.update(time.time() - end)
+                end = time.time()
+                
                 pbar.set_description("[Task:{}|Epoch:{}] Avg Loss: {:.5}".format(task_id+1, epoch+1, losses.avg))
+                wandb.log({'Batch Time': batch_time.avg})
+                # Memory Usage in MB
+                memory_info = psutil.virtual_memory()
+                memory_mb = memory_info.used / (1024 ** 2)
+                wandb.log({'Memory Usage(MB) in iter': memory_mb})
             
             if scheduler is not None:
                 scheduler.step()
@@ -100,6 +153,8 @@ def main(args):
         #- Start Evaluation
         accs = test_evaluators[0].fit(current_task_id=task_id, logger=None)
         metrics.append(accs)
+        wandb.log({'Task-Verif ACC': accs})
+        # wandb.log('Task-{task_id}-Verif ACC: {acc}'.format(task_id=task_id, acc=accs), step=epoch)
     
         if args.save_path is not None:
             if args.save_model:
@@ -114,15 +169,21 @@ def main(args):
                 
     
 if __name__ == "__main__":
-    class Config:
-        seed = 0
-        batch_size = 512
-        lr = 0.01
-        nowand = 1
-        n_epochs = 20
-        debug = 0
-        save_path = 'save/casia_15'
-        save_model = 1
+    # class Config:
+    #     target_device = 'pc'
+    #     seed = 0
+    #     n_workers = 4
+    #     batch_size = 512
+    #     lr = 0.01
+    #     nowand = 0
+    #     n_epochs = 20
+    #     debug = 0
+    #     save_path = 'save/casia_15'
+    #     save_model = 1
+    #     wandb_project ='nano'
+    #     wandb_entity ='laymond1'
+    # args = Config
 
-    args = Config
+    args = parse_args()
+    
     main(args)
